1aaea2690a3a3ba5da67956fe82c249df4359610
Leandro Villalba <villalbaleandrogabriel@gmail.com>
2025-09-09 10:11:24 -0300

    Culminación de ejercicio (#7)

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/Lelo88/go-mysql-example/database"
 	"github.com/Lelo88/go-mysql-example/handlers"
-	// "github.com/Lelo88/go-mysql-example/models"
+	"github.com/Lelo88/go-mysql-example/models"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -18,8 +21,8 @@ func main() {
 
 	defer db.Close()
 
-	handlers.ListContacts(db)
-	fmt.Println("-----")
+	/* handlers.ListContacts(db)
+	fmt.Println("-----") */
 	/* fmt.Println("Get contact with ID 1:")
 	handlers.GetContactByID(db, 1)
 
@@ -46,8 +49,92 @@ func main() {
 	handlers.UpdateContact(db, updatedContact)
 	handlers.ListContacts(db) */
 
-	fmt.Println("-----")
+	/* fmt.Println("-----")
 	fmt.Println("Delete contact with ID 1:")
 	handlers.DeleteContact(db, 5)
-	handlers.ListContacts(db)
+	handlers.ListContacts(db) */
+
+	for {
+		fmt.Println("\n MENU")
+		fmt.Println("1. List Contacts")
+		fmt.Println("2. Get Contact by ID")
+		fmt.Println("3. Create Contact")
+		fmt.Println("4. Update Contact")
+		fmt.Println("5. Delete Contact")
+		fmt.Println("6. Exit")
+		var choice int
+		fmt.Print("Enter your choice: ")
+		_, err := fmt.Scan(&choice)
+		if err != nil {
+			fmt.Println("Invalid input, please enter a number.")
+			continue
+		}
+		
+		switch choice {
+		case 1:
+			handlers.ListContacts(db)
+		case 2:
+			var id int
+			fmt.Print("Enter Contact ID: ")
+			_, err := fmt.Scan(&id)
+			if err != nil {
+				fmt.Println("Invalid input, please enter a number.")
+				continue
+			}
+			handlers.GetContactByID(db, id)
+		case 3:
+			contact := inputContactDetails(3)
+			handlers.CreateContact(db, contact)
+		case 4:
+			contact := inputContactDetails(4)
+			handlers.UpdateContact(db, contact)
+		case 5:
+			var id int
+			fmt.Print("Enter Contact ID to delete: ")
+			_, err := fmt.Scan(&id)
+			if err != nil {
+				fmt.Println("Invalid input, please enter a number.")
+				continue
+			}
+			handlers.DeleteContact(db, id)
+		case 6:
+			fmt.Println("Exiting...")
+			return
+		}
+	}
+}
+
+func inputContactDetails(option int) models.Contact {
+	reader := bufio.NewReader(os.Stdin)
+
+	var contact models.Contact
+
+	if option == 3 {
+		fmt.Print("Enter ID: ")
+		var id int
+		fmt.Scan(&id)
+		contact.ID = id
+	}
+
+	if option == 4 {
+		fmt.Print("Enter ID of the contact to update: ")
+		var id int
+		fmt.Scan(&id)
+		contact.ID = id
+	}
+
+	fmt.Print("Enter Name: ")
+	name, _ := reader.ReadString('\n')
+	contact.Name = strings.TrimSpace(name)
+
+	fmt.Print("Enter Email (or leave blank): ")
+	email, _ := reader.ReadString('\n')
+	contact.Email = strings.TrimSpace(email)
+
+	fmt.Print("Enter Phone: ")
+	phone, _ := reader.ReadString('\n')
+	contact.Phone = strings.TrimSpace(phone)
+
+	return contact
+
 }
