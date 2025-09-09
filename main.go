2edf3b9a2971e48879bca3f19cbcc5436409ed35
Leandro Villalba <villalbaleandrogabriel@gmail.com>
2025-09-09 09:09:51 -0300

    Agregado de contacto

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Lelo88/go-mysql-example/database"
 	"github.com/Lelo88/go-mysql-example/handlers"
+	"github.com/Lelo88/go-mysql-example/models"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -21,4 +22,16 @@ func main() {
 	fmt.Println("-----")
 	fmt.Println("Get contact with ID 1:")
 	handlers.GetContactByID(db, 1)
+
+	fmt.Println("-----")
+	
+	newContact := models.Contact{
+		Name:  "John Doe",
+		Email: "",
+		Phone: "123-456-7890",
+	}
+	handlers.CreateContact(db, newContact)
+
+	fmt.Println("-----")
+	handlers.ListContacts(db)
 }
